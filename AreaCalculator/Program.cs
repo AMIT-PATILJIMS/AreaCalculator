838c5a2e756feb71b5a9496abfddc4fd3d2571e9
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-07-08 12:11:44 +0530

    Add project files.

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    public static class Program
+    {
+        public static int Rectangle(int length,int breadth)
+        {
+            return length * breadth;
+        }
+        public static double Circle(int radius)
+        {
+            return 3.14 * radius * radius;
+        }
+        public static int Square(int side)
+        {
+            return side * side;
+        }
+        public static int Triangle(int baseOfTriangle, int height)
+        {
+            return baseOfTriangle * height / 2;
+        }
+    }
+}
